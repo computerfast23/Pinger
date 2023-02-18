695d16dd5ba2c3d0480f1d60e3ed8d3e991a8e33
computerfast23 <88633433+computerfast23@users.noreply.github.com>
2023-02-18 12:30:58 -0600

    unnecessary code removed

--- Pinger.bat
+++ Pinger.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCuDJGyX8VAjFDxBXxCDKmT6V4k45//14+WGpl4hQ+s5cbDI07OAIews/k3heJkj325VivRZVUIMMBuoYW8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdFy5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSTk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
-::ZQ05rAF9IAHYFVzEqQIAIRVTSRbi
-::eg0/rx1wNQPfEVWB+kM9LVsJDDSLMmy/FNU=
-::fBEirQZwNQPfEVWB+kM9LVsJDDSLMmy/FNU=
-::cRolqwZ3JBvQF1fEqQITIB5XR0SWNG76FrwS76mb
-::dhA7uBVwLU+EWG2M8UFiewtGQw6HP3+pZg==
-::YQ03rBFzNR3SWATEwEo1K0kHXBaNNm65EqZ8
-::dhAmsQZ3MwfNWATEwEo1K0kHXBaNNm65EqZ8
-::ZQ0/vhVqMQ3MEVWAtB9wHBNRT1bRLHm1DLAf/Pub
-::Zg8zqx1/OA3MEVWAtB9wHBNRT1bRLHm1DLAf/Pub
-::dhA7pRFwIByZRRmq+wI4KRlGTRfCOXiuA/Ud+uvz5vyI4l4UFOwwOJvS37qLM6Aa5FHhZ5gowjs6
-::Zh4grVQjdCuDJGyX8VAjFDxBXxCDKmT6V4k47fvw++WXnn0UWuU6asHewrHu
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 @mode con cols=60 lines=20
 title Pinger - Medidor de ms  
